98ef8f0eb0db1ddd998b19f63c7a3b3e35984379
e-tayfw <ethantay321@gmail.com>
2025-06-10 01:49:35 +0800

    chore: change photo directory

--- src/data/memories.ts
+++ src/data/memories.ts
@@ -15,10 +15,10 @@ export const memories: Memory[] = [
     description:
       "Attended my first football match in Madrid, witnessing the passion of the fans and the excitement of the game.",
     image: [
-      "/src/assets/images/hala_madrid.jpeg",
-      "/src/assets/images/realMadrid1.jpeg",
-      "/src/assets/images/realMadrid2.jpeg",
-      "/src/assets/images/realMadrid3.jpeg",
+      "/src/public/images/hala_madrid.jpeg",
+      "/src/public/images/realMadrid1.jpeg",
+      "/src/public/images/realMadrid2.jpeg",
+      "/src/public/images/realMadrid3.jpeg",
     ],
     date: "2025-05-14",
   },
@@ -28,9 +28,9 @@ export const memories: Memory[] = [
     country: "Granada",
     description:
       "Wandered through the beautiful outskirts of Granada, enjoying the scenic views and rich history through mountain biking.",
-    image: ["/src/assets/images/bike_grp_photo.jpeg",
-        "/src/assets/images/granadaBike1.jpeg",
-        "/src/assets/images/granadaBike2.jpeg"
+    image: ["/src/public/images/bike_grp_photo.jpeg",
+        "/src/public/images/granadaBike1.jpeg",
+        "/src/public/images/granadaBike2.jpeg"
     ],
     date: "2025-05-03",
   },
@@ -40,8 +40,8 @@ export const memories: Memory[] = [
     country: "Granada",
     description:
       "Enjoy local Andalusian cuisine at a charming restaurant in Granada, beside the Alhambra. Love the tapas culture and affordable prices here!",
-    image: ["/src/assets/images/granada_first_meal.jpeg",
-        "/src/assets/images/granada_first_meal2.jpeg",
+    image: ["/src/public/images/granada_first_meal.jpeg",
+        "/src/public/images/granada_first_meal2.jpeg",
     ],
     date: "2025-05-02",
   },
